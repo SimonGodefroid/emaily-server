0d11ec0ab816912c070447f773fe3e9662c12bd5
SimonGodefroid <simon.godefroid@gmail.com>
2017-11-20 21:19:42 +0100

    added proxy true and removed keys ahem

--- config/prod.js
+++ config/prod.js
@@ -6,6 +6,3 @@ module.exports = {
 	mongoURI: process.env.MONGO_URI,
 	cookieKey: process.env.COOKIE_KEY
 };
-
-// 167507390779-o1oj5s22cjupjqkpid9jtdobrtu8qo7a.apps.googleusercontent.com
-// 6m4sNUM7cNIUc5OTNKln3FHE
