66f0ba73f425f7461ccf400d41691f87a07cf100
SimonGodefroid <simon.godefroid@gmail.com>
2017-11-20 18:18:25 +0100

    added user instance creation logic

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,8 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+const User = mongoose.model('users');
 passport.use(
 	new GoogleStrategy(
 		{
@@ -9,9 +11,14 @@ passport.use(
 			callbackURL: '/auth/google/callback'
 		},
 		(accessToken, refreshToken, profile, done) => {
-			console.log('accessToken', accessToken);
-			console.log('refreshToken', refreshToken);
-			console.log('profile', profile);
+			User.findOne({ googleId: profile.id }).then(existingUser => {
+				if (existingUser) {
+					// we already have a record with the given profile ID
+				} else {
+					// we don't have a user record with this ID, make a new one and save it
+					new User({ googleId: profile.id }).save();
+				}
+			});
 		}
 	)
 );
