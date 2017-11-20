537a0e7ca20b4aa9c86a54c5f6b99fd7586ff22d
SimonGodefroid <simon.godefroid@gmail.com>
2017-11-20 15:42:51 +0100

    connected mongoose

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
 	googleClientID: '925659333375-trp1f6bjt7guvoe6ph60rsmfkhmpb7a9.apps.googleusercontent.com',
-	googleClientSecret: '7IEJjNkQ-rkZ-i3by8zS06YN'
+	googleClientSecret: '7IEJjNkQ-rkZ-i3by8zS06YN',
+	mongoURI: 'mongodb://simon:password@ds135574.mlab.com:35574/emaily-dev'
 };
