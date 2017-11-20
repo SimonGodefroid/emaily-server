ab0e886032fa97b523a5e494d106261716704d14
SimonGodefroid <simon.godefroid@gmail.com>
2017-11-20 20:07:05 +0100

    added logout route

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
 	googleClientID: '925659333375-trp1f6bjt7guvoe6ph60rsmfkhmpb7a9.apps.googleusercontent.com',
 	googleClientSecret: '7IEJjNkQ-rkZ-i3by8zS06YN',
-	mongoURI: 'mongodb://simon:password@ds135574.mlab.com:35574/emaily-dev'
+	mongoURI: 'mongodb://simon:password@ds135574.mlab.com:35574/emaily-dev',
+	cookieKey: 'Simon@ppSuperSecretCookieKey'
 };
--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
-mongoose.connect(keys.mongoURI);
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI, { useMongoClient: true });
 
 const app = express();
+
+app.use(
+	// sets the life expentancy of cookies
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000, // 30days
+		keys: [keys.cookieKey]
+	})
+);
+// tells passport to initialize for config
+app.use(passport.initialize());
+// tells passport to use session
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
