ccbcca58ba17105b703ad54d59289cf732cbaeb7
SimonGodefroid <simon.godefroid@gmail.com>
2017-11-21 17:20:18 +0100

    added routing after login

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
 class Header extends Component {
+	renderContent() {
+		switch (this.props.auth) {
+			case null:
+				return;
+			case false:
+				return (
+					<li>
+						<a href="/auth/google">Login With Google</a>
+					</li>
+				);
+			default:
+				return (
+					<li>
+						<a href="">Logout</a>
+					</li>
+				);
+		}
+	}
 	render() {
+		console.log(this.props);
 		return (
 			<nav>
 				<div className="nav-wrapper">
@@ -10,11 +29,7 @@ class Header extends Component {
 							<a href="/" className="left brand-logo">
 								Emaily
 							</a>
-							<ul className="right">
-								<li>
-									<a>Login with Google</a>
-								</li>
-							</ul>
+							<ul className="right">{this.renderContent()}</ul>
 						</div>
 					</div>
 				</div>
@@ -23,4 +38,7 @@ class Header extends Component {
 	}
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+	return { auth };
+}
+export default connect(mapStateToProps)(Header);
