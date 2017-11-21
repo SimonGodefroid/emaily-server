ecd47176d69ba5653bfb6ac699a53af05986106d
SimonGodefroid <simon.godefroid@gmail.com>
2017-11-21 11:27:04 +0100

    refactored the passport google strategy with async await synthax rather than promises

--- services/passport.js
+++ services/passport.js
@@ -15,6 +15,31 @@ passport.deserializeUser((id, done) => {
 	});
 });
 
+// promise based code
+// passport.use(
+// 	new GoogleStrategy(
+// 		{
+// 			clientID: keys.googleClientID,
+// 			clientSecret: keys.googleClientSecret,
+// 			callbackURL: '/auth/google/callback',
+// 			proxy: true
+// 		},
+// 		(accessToken, refreshToken, profile, done) => {
+// 			User.findOne({ googleId: profile.id }).then(existingUser => {
+// 				if (existingUser) {
+// 					// we already have a record with the given profile ID
+// 					// null = no error and we return the user
+// 					done(null, existingUser);
+// 				} else {
+// 					// we don't have a user record with this ID, make a new one and save it
+// 					new User({ googleId: profile.id }).save().then(user => done(null, user));
+// 				}
+// 			});
+// 		}
+// 	)
+// );
+
+// async await refactor
 passport.use(
 	new GoogleStrategy(
 		{
@@ -23,17 +48,13 @@ passport.use(
 			callbackURL: '/auth/google/callback',
 			proxy: true
 		},
-		(accessToken, refreshToken, profile, done) => {
-			User.findOne({ googleId: profile.id }).then(existingUser => {
-				if (existingUser) {
-					// we already have a record with the given profile ID
-					// null = no error and we return the user
-					done(null, existingUser);
-				} else {
-					// we don't have a user record with this ID, make a new one and save it
-					new User({ googleId: profile.id }).save().then(user => done(null, user));
-				}
-			});
+		async (accessToken, refreshToken, profile, done) => {
+			const existingUser = await User.findOne({ googleId: profile.id });
+			if (existingUser) {
+				return done(null, existingUser);
+			}
+			const user = await new User({ googleId: profile.id }).save();
+			done(null, user);
 		}
 	)
 );
