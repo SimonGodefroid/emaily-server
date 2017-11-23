33187e9f0b6f08ee359cc78d16f88a47ba29e858
SimonGodefroid <simon.godefroid@gmail.com>
2017-11-23 12:07:41 +0100

    added references in the survey model

--- models/Survey.js
+++ models/Survey.js
@@ -7,6 +7,9 @@ const surveySchema = new Schema({
 	body: String,
 	recipients: [RecipientSchema],
 	yes: { type: Number, default: 0 },
-	no: { type: Number, default: 0 }
+	no: { type: Number, default: 0 },
+	_user: { type: Schema.Types.ObjectId, ref: 'User' }, // reference field
+	dateSent: Date,
+	lastResponded: Date
 });
 mongoose.model('surveys', surveySchema);
