b1a908b3fef8fc8e8f3dd32f408a52c27438b9bd
SimonGodefroid <simon.godefroid@gmail.com>
2017-11-23 15:47:09 +0100

    defined mailer class

--- services/Mailer.js
+++ services/Mailer.js
@@ -3,6 +3,15 @@ const helper = sendgrid.mail;
 const keys = require('../config/keys');
 
 // helper.Mail takes options and spits out an object
-class Mailer extends helper.Mail {}
+class Mailer extends helper.Mail {
+	constructor({ subject, recipients }, content) {
+		super();
+
+		this.from_email = new helper.Email('no-reply@emaily.com');
+		this.subject = subject;
+		this.body = new helper.Content('text/html', content);
+		this.recipients = this.formatAddresses(recipients);
+	}
+}
 
 module.exports = Mailer;
