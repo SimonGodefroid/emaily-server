b012f3ded46cd455b14f92898ab89ef9a2dbc58f
SimonGodefroid <simon.godefroid@gmail.com>
2017-11-23 16:00:21 +0100

    setting up the mailer

--- services/Mailer.js
+++ services/Mailer.js
@@ -6,11 +6,26 @@ const keys = require('../config/keys');
 class Mailer extends helper.Mail {
 	constructor({ subject, recipients }, content) {
 		super();
-
 		this.from_email = new helper.Email('no-reply@emaily.com');
 		this.subject = subject;
 		this.body = new helper.Content('text/html', content);
 		this.recipients = this.formatAddresses(recipients);
+		this.addContent(this.body);
+		this.addClickTracking();
+		this.addRecipients();
+	}
+	formatAddresses(recipients) {
+		return recipients.map(({ email }) => {
+			return new helper.Email(email);
+		});
+	}
+	addClickTracking() {
+		// from send grid documentation
+		const trackingSettings = new helper.TrackingSettings();
+		const clickTracking = new helper.ClickTracking(true, true);
+
+		trackSettings.setClickTracking(clickTracking);
+		this.addTrackingSettings(trackingSettings);
 	}
 }
 
