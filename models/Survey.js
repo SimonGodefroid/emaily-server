f118e70c541c3a703c21d41bd869c676be3244ae
SimonGodefroid <simon.godefroid@gmail.com>
2017-11-23 12:02:22 +0100

    added recipient schema

--- models/Survey.js
+++ models/Survey.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose; // short for const Schema = mongoose.Schema;
+const RecipientSchema = require('./Recipient');
 const surveySchema = new Schema({
 	title: String, // title of the campaign
 	subject: String,
 	body: String,
-	recipients: [String],
+	recipients: [RecipientSchema],
 	yes: { type: Number, default: 0 },
 	no: { type: Number, default: 0 }
 });
