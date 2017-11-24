3eebd8fd12fdecbf5409aa79fcb4f3bb9cbf7cc9
SimonGodefroid <simon.godefroid@gmail.com>
2017-11-24 14:48:27 +0100

    added fields

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,10 +1,31 @@
 // SurveyForm shows a form for a user to add input
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
+import SurveyField from './SurveyField';
 class SurveyForm extends Component {
+	renderFields() {
+		return (
+			<div>
+				<Field name={`title`} label={`Survey Title`} type={`text`} component={SurveyField} />
+				<Field name={`subject`} label={`Subject Line`} type={`text`} component={SurveyField} />
+				<Field name={`body`} label={`Email Body`} type={`text`} component={SurveyField} />
+				<Field name={`emails`} label={`Recipients List`} type={`text`} component={SurveyField} />
+			</div>
+		);
+	}
 	render() {
-		return <div>SurveyForm !</div>;
+		return (
+			<div>
+				<form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+					{this.renderFields()}
+					<button className={``} type={`submit`}>
+						Submit
+					</button>
+				</form>
+			</div>
+		);
 	}
 }
 
+// handleSubmit comes from surveyForm
 export default reduxForm({ form: 'surveyForm' })(SurveyForm);
