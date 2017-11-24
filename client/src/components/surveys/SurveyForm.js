c1f762e52a2417fee6e9fac891606a6c8b48ae0e
SimonGodefroid <simon.godefroid@gmail.com>
2017-11-24 16:08:59 +0100

    created util to filter out faulty emails

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,15 +1,15 @@
 // SurveyForm shows a form for a user to add input
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
-import _ from 'lodash';
-
+import validateEmails from '../../utils/validateEmails';
 const FIELDS = [
 	{ label: `Survey Title`, name: `title` },
 	{ label: `Subject Line`, name: `subject` },
 	{ label: `Email Body`, name: `body` },
-	{ label: `Recipients List`, name: `emails` }
+	{ label: `Recipient List`, name: `emails` }
 ];
 class SurveyForm extends Component {
 	renderFields() {
@@ -38,6 +38,7 @@ class SurveyForm extends Component {
 
 function validate(values) {
 	const errors = {};
+	errors.emails = validateEmails(values.emails || '');
 	_.each(FIELDS, ({ name }) => {
 		if (!values[name]) {
 			errors[name] = `You must provide ${name}!`;
