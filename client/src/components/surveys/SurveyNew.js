f5ed43652f295db3e864ffa35dac9b1aaa34edea
SimonGodefroid <simon.godefroid@gmail.com>
2017-11-24 16:39:44 +0100

    added back button

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -12,7 +12,7 @@ class SurveyNew extends Component {
 	state = { showFormReview: false };
 	renderContent() {
 		if (this.state.showFormReview) {
-			return <SurveyFormReview />;
+			return <SurveyFormReview onCancel={() => this.setState({ showFormReview: false })} />;
 		}
 		return <SurveyForm onSurveySubmit={() => this.setState({ showFormReview: true })} />;
 	}
