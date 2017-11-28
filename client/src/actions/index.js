7f8d077a7c30c58dbf6d614023acb1a97e78442b
SimonGodefroid <simon.godefroid@gmail.com>
2017-11-28 18:30:55 +0100

    created and wired action creator and reducer for surveys

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { FETCH_USER } from './types';
+import { FETCH_USER, FETCH_SURVEYS } from './types';
 
 // async await based
 // if there is only one line to the arrow function codeblock we can remove the {} and the return
@@ -17,6 +17,11 @@ export const submitSurvey = (values, history) => async dispatch => {
 	const res = await axios.post('/api/surveys/', values);
 	history.push('/surveys');
 	dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const fetchSurveys = () => async dispatch => {
+	const res = await axios.get('/api/surveys/');
+	dispatch({ type: FETCH_SURVEYS, payload: res.data });
 };
 
 //
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
+import surveysReducer from './surveysReducer';
 import { reducer as reduxForm } from 'redux-form';
 // the keys of this object will be the keys inside the state object
 export default combineReducers({
 	auth: authReducer,
-	form: reduxForm
+	form: reduxForm,
+	surveys: surveysReducer
 });
