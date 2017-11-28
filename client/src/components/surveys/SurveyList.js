b2753f0d5e9b83892832f0838a209689cfa82fa0
SimonGodefroid <simon.godefroid@gmail.com>
2017-11-28 19:17:18 +0100

    added survey list on the dashboard

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -6,14 +6,34 @@ class SurveyList extends Component {
 	componentDidMount() {
 		this.props.fetchSurveys();
 	}
+
+	renderSurveys() {
+		return this.props.surveys.reverse().map(survey => {
+			return (
+				<div className={`card darken-1`} key={survey._id}>
+					<div className={`card-content`}>
+						<span className={`card-title`}>{survey.title}</span>
+						<p>{survey.body}</p>
+						<p className={`right`}>Sent On: {new Date(survey.dateSent).toLocaleDateString()}</p>
+					</div>
+					<div className={`card-action`}>
+						<a>Yes: {survey.yes}</a>
+						<a>No: {survey.no}</a>
+					</div>
+				</div>
+			);
+		});
+	}
 	render() {
-		return <div>SurveyList</div>;
+		if (this.props.surveys && this.props.surveys.length > 0) {
+			return <div>{this.renderSurveys()}</div>;
+		} else {
+			return <div>Nothing here</div>;
+		}
 	}
 }
 
 function mapStateToProps({ surveys }) {
-	return {
-		surveys
-	};
+	return { surveys };
 }
 export default connect(mapStateToProps, { fetchSurveys })(SurveyList);
